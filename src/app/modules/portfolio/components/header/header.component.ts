a7a5ee653a81c72f2a06debfd07dac387b5fc19f
Imílio Manuel <emiliomdmanuel12@gmail.com>
2025-01-29 13:16:04 +0100

    portfolio terminado terminado

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private document = inject(DOCUMENT);
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollTo(elementId: string) {
+    this.document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
+    this.isMenuOpen = false;
+  }
 }
--- src/app/modules/portfolio/pages/portfolio-page/portfolio-page.component.ts
+++ src/app/modules/portfolio/pages/portfolio-page/portfolio-page.component.ts
@@ -1,67 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
-import { PrimaryBtnComponent } from "../../components/primary-btn/primary-btn.component";
-import { CardComponent } from "../../components/card/card.component";
+
 
 @Component({
   selector: 'app-portfolio-page',
   imports: [
-    HeaderComponent,
-    PrimaryBtnComponent, 
-    CardComponent,
+    HeaderComponent
   ],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.scss'
 })
-export class PortfolioPageComponent implements OnInit {
-  
-  btn_content_hero = 'Entrar em contacto';
-  btn_content_about = 'Download Resume';
-  
-  heroSubtitle = '';
-  fadeOut: boolean = false;
-  
-  heroMessages: string[] = [
-    'Desenvolvedor Full-Stack',
-    'Implentação de Soluções Open Source',
-    'Analista de Sistemas Linux',
-    'Devops'
-  ];
+export class PortfolioPageComponent {
 
-  cards = [
-    {
-      card_img: '/modules/portfolio/services/svg/spring-boot.svg',
-      card_title: 'Angular',
-      card_description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-      card_img: '/modules/portfolio/services/svg/react.svg',
-      card_title: 'React',
-      card_description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-      card_img: '/modules/portfolio/services/svg/vue.svg',
-      card_title: 'Vue.js',
-      card_description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-      card_img: '/modules/portfolio/services/svg/nodejs.svg',
-      card_title: 'Node.js',
-      card_description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    }
-  ];
-  
-  ngOnInit(): void {
-    let index = 0;
-    this.heroSubtitle = this.heroMessages[index];
-    setInterval(() => {
-      this.fadeOut = true;
-      setTimeout(() => {
-        index = (index + 1) % this.heroMessages.length; 
-        this.heroSubtitle = this.heroMessages[index]; 
-        this.fadeOut = false;
-      }, 1000); 
-    }, 4000); 
-  }
   
 }
